08b042127427ba43432cd0f8ad2f05539c07760f
Konstantinos Kanellis <kkanelli@hotmail.com>
2014-03-08 14:38:08 +0200

    Added list comprehension workshop

---
+++ Workshop/list_comprehensions.py
@@ -0,0 +1,47 @@
+# List-Set-Dict comprehension exercises
+# - - - - - - - - - - - - - - - - -
+
+# Write a comprehension whose values is the set consisting
+# of the squares of the first ten positive integers.
+def squares:
+    # write code here #
+    return
+
+# Write a comprehension whose values is the first 10 powers of 2
+def bin_power:
+    # write code here #
+    return
+
+# Write a list comprehension whose value is the cartesian product
+# of two lists A , B.
+# Example: A = [1,2,3], B = [4,5,6]:
+#       A*B = [ (1,4), (1,5), (1,6), (2,4), (2,5), (2,6), (3,4), (3,5), (3,6)
+def cartesian_product(A, B):
+    # write code here #
+    return
+
+# Write a function which takes two lists A,B and creates using comprehension
+# a dictionary whose keys is the items of list A and whose values
+# are the items of list B. ( Implement zip built-in function )
+# Example: A = ['A','B','C'], B = [ 5.0, 7.5, 3.14 ]
+#       returns: { 'A':5.0, 'B':7.5, 'C':3.14 }
+def zip_lists(A, B):
+    # write code here #
+    return
+
+# Write a comprehension whose values is the set consisting
+# of the odd numbers up to 100
+def odd_list:
+    # write code here #
+    return
+
+# Write a function which takes a list of positive numbers and returns
+# a list that consist of: twice of that number if that number is divisible by 3
+#                          square of that number if that number is not.
+# Example: A = [3, 2, 10,   7,  9, 5 ]
+#     returns: [6, 4, 100, 49, 18, 10]
+def filter_list(A):
+    # write code here #
+    return
+
+
